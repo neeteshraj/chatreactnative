beca9327badf5165bc94c9624ab9cc42b437620d
Nitesh Raj Khanal <nitesh.khanal19@gmail.com>
2022-11-26 18:53:05 +0545

    feat: :wrench: baseurl changed from localhost to herokuu deployed application
    base url has been updated from localhost to herokuu deployed backend application to make requests for both ios and android application.

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -1,47 +1,27 @@
 import axios from 'axios';
-import {Platform} from 'react-native';
 const getUser = async () => {
-  if (Platform.OS === 'ios') {
-    try {
-      const response = await axios.get('http://localhost:3000/users');
-      return response.data;
-    } catch (error) {
-      console.log('Error while fetching data =>', error);
-    }
-  }
-
-  if (Platform.OS === 'android') {
-    try {
-      const response = await axios.get('http://10.0.2.2:3000/users');
-      return response.data;
-    } catch (error) {
-      console.log('Error while fetching data => ', error);
-    }
+  try {
+    const response = await axios.get(
+      'https://evening-anchorage-59874.herokuapp.com/users',
+    );
+    return response.data;
+  } catch (error) {
+    console.log('Error while fetching data =>', error);
   }
 };
 
 const loginApi = async (email: string, password: string) => {
-  if (Platform.OS === 'ios') {
-    try {
-      const response = await axios.post('http://localhost:3000/login', {
+  try {
+    const response = await axios.post(
+      'https://evening-anchorage-59874.herokuapp.com/login',
+      {
         email,
         password,
-      });
-      return response.data;
-    } catch (error) {
-      console.log('Error while logging in =>', error);
-    }
-  }
-  if (Platform.OS === 'android') {
-    try {
-      const response = await axios.post('http://10.0.2.2:3000/login', {
-        email,
-        password,
-      });
-      return response.data;
-    } catch (error) {
-      console.log('Error while logging in => ', error);
-    }
+      },
+    );
+    return response.data;
+  } catch (error) {
+    console.log('Error while logging in => ', error);
   }
 };
 
